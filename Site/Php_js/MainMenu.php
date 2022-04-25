b52acd855fb7af8a37277bffee8381f1f8ffdff7
sagmax <98807415+sagmax@users.noreply.github.com>
2022-04-26 01:24:26 +0400

    Add files via upload

---
+++ Site/Php_js/MainMenu.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>MainMenu</title>
+    <link href='http://fonts.googleapis.com/css?family=Stalinist+One&amp;amp;amp;subset=cyrillic' rel='stylesheet' type='text/css' >
+    <link rel="stylesheet" href="/Site/Css/MainMenu.css">
+</head>
+
+<body>
+<div class="Main">
+    <div class="column1">
+        <a href="Oblozhka.php" class="b">ProMenya</a>
+    </div>
+    <div class="column2">
+        <a href="MainMenu.php" class="ctr"> Главное меню </a>
+        <a href="Mlidm.php" class="ctr"> Лабы по Млидм </a>
+        <a href="Gallery.php"class="ctr">Галерея</a> 
+        <a href="Contacts.php"class="ctr">Контакты</a> 
+        <a href="Avtorizacia.php"class="ctr">Вход</a>
+        <div class="colocol" onclick="alert('динь-динь')">
+            <img src='/Site/Img/icon1.png'>
+        </div>   
+    </div>
+</div>
+<canvas id="canvas"></canvas>
+</div>
+<script src="Matrix.js"></script>
+<div class="Tema">
+    <div class="Photo">
+        <img class="Iam" src='/Site/Img/MyPhoto.jpg'>
+    </div>
+    <div class="Cherta">
+       
+    </div>
+    <div class="Text">
+        <div class="MainStroka">
+            Здесь вы узнаете что же я люблю делать..
+        </div>
+        <div class="Stroki">
+        <br><br><br>
+            <th> 1. УСТРАИВАТЬ БЕЗУМНЫЕ ГОНКИ </th><br>
+            <br><br><br><br>
+            <th> 2. ИЗОБРАЖАТЬ НА БУМАГЕ ПЛАН ЗАХВАТА ПЛАНЕТЫ</th><br>
+            <br><br><br><br>
+            <th> 3. ЛАЗИТЬ ПО ЛЕСТНИЦЕ</th><br><br><br><br><br>
+
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
