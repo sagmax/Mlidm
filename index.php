6807333682fa84759983f43d673b02ef8dafa514
sagmax <98807415+sagmax@users.noreply.github.com>
2022-04-26 16:49:30 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Лабораторные работы</title>
+    <link rel="stylesheet" href="/Site/Css/wow.css">
+</head>
+
+<body>
+    <h1> Мои лабораторные работы</h1>
+    <t>Дискретная математика:<r>Интернет программирование:</r></t>
+    <a href="/Mlidm/MlidmLab1.php"><div class="mlidm"> Лаб 1</a>  <a href="/Site/Php_js/Oblozhka.php"><g>Мой Сайт</g></a><br></div>
+    <a href="/Mlidm/MlidmLab2.php"><div class="mlidm"> Лаб 2</div></a>
+    <q>
+    <img src='https://media2.giphy.com/media/S9RKaj4E4dhWvKfMHf/giphy.gif?cid=ecf05e47m2ixbetdqqs3baj7a2pc84owt4aqw3nr0y28b72n&rid=giphy.gif&ct=g'>
+    </q>
+</body>
+
+</html>
