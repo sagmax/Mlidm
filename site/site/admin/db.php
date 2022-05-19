9e2eebd63ddad1b91ffb2a80838fca0302a22244
sagmax <98807415+sagmax@users.noreply.github.com>
2022-05-19 18:01:37 +0400

    Delete site/site directory

--- site/site/admin/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-require "rb.php";
-R::setup('mysql:host=localhost; dbname=bd_ulstu', 'root', '');
-session_start();
-?>
--- site/site/php_js/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require 'db.php';
-    unset($_SESSION['logged_user']);
-    header('location: /site/php_js/oblozhka.php');
-?>
